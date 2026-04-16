c78fa19beed4f0f380c11e36138742958b7b19a4
Chukwu Sydney <chukwusydney061@gmail.com>
2026-04-16 16:01:45 +0100

    Create Product API

---
+++ eCommerce.Product/ProductApi.Application/DTOs/Conversions/ProductConversion.cs
@@ -0,0 +1,53 @@
+﻿using ProductApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi.Application.DTOs.Conversions
+{
+    public static class ProductConversion
+    {
+        public static Product ToEntity(ProductDTO product) => new()
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Quantity = product.Quantity,
+            Price = product.Price
+        };
+
+        public static (ProductDTO?, IEnumerable<ProductDTO>?) FromEnntity(Product product, IEnumerable<Product>? products)
+        {
+            //RETURN SINGLE
+            if(product is not null || product is null)
+            {
+                var singleProduct = new ProductDTO
+                    (product!.Id,
+                    product.Name!,
+                    product.Quantity,
+                    product.Price
+                    );
+                return (singleProduct, null);
+            }
+
+            //return list
+            if (product is not null || product is null)
+            {
+                var _products = products.Select(p => 
+                    new ProductDTO
+                    (
+                    p.Id,
+                    p.Name!,
+                    p.Quantity,
+                    p.Price
+                    )).ToList();
+
+                return (null, _products);
+
+            }
+
+            return (null, null);
+        }
+    }
+}
