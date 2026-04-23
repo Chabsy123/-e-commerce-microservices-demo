89089e11205c5c0513ca0d700764122043825065
Chukwu Sydney <chukwusydney061@gmail.com>
2026-04-23 03:35:46 +0100

    feat: Implement Auth API, DTOs and User interface

--- eCommerce.Authentication/AuthenticationApi.Domain/Entities/AppUser.cs
+++ eCommerce.Authentication/AuthenticationApi.Domain/Entities/AppUser.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AuthenticationApi.Domain.Entities
+﻿namespace AuthenticationApi.Domain.Entities
 {
-    internal class AppUser
+    public class AppUser
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+       
+        public string? TelephoneNumber { get; set; }
+
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Role { get; set; }
+        public DateTime DateRegistered { get; set; } = DateTime.UtcNow;
     }
 }
